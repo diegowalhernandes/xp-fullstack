e2ba98e2f6853395114073f6ec557e516ec91c7a
Diego Hernandes <diegowalhernandes@gmail.com>
2024-08-31 17:50:11 -0300

    incremento decremento

--- Fundamentos/Program.cs
+++ Fundamentos/Program.cs
@@ -2,13 +2,24 @@
 
 Calculadora calc = new Calculadora();
 
-calc.Somar(10, 30);
-calc.Subtrair(10, 50);
-calc.Multiplicar(15, 45);
-calc.Dividir(2, 2);
+// calc.Somar(10, 30);
+// calc.Subtrair(10, 50);
+// calc.Multiplicar(15, 45);
+// calc.Dividir(2, 2);
+// calc.Potencia(3, 3);
+// calc.Seno(30);
+// calc.Coseno(30);
+// calc.Tangente(30);
 
-calc.Potencia(3, 3);
+int numeroIncremento = 10;
 
-calc.Seno(30);
-calc.Coseno(30);
-calc.Tangente(30);
+Console.WriteLine(numeroIncremento);
+Console.WriteLine("Incrementando");
+numeroIncremento ++;
+Console.WriteLine(numeroIncremento);
+
+int numeroDecremento = 10;
+Console.WriteLine("Decrementando");
+numeroDecremento --;
+Console.WriteLine(numeroDecremento);
+
